5951df407fd2a210f5807c6fbbe743c5c1a4272b
Kasun Hettiaracchi <kasunhettiaracchi@Kasuns-iMac.local>
2024-05-09 14:32:39 +0400

    CRUD get with bootrap

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Student;
+
+class StudentController extends Controller
+{
+    public function index()
+    {
+        $students = Student::all();
+
+        return view('students.index', compact('students'));
+    }
+}
